c748fb2a1ba52a8e6bbf6bb8eaaa4f717311317a
Irvin Capagcuan <irvin@capagcuan.org>
2020-08-17 10:34:39 +0800

    updates

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/playground
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-brew install ansible
+if [[ ! -a "${HOME}/.oh-my-zsh" ]]
+then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi;
+
+if ! command -v brew &> /dev/null
+then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+if ! command -v ansible &> /dev/null
+then
+    brew install ansible
+fi
 
 ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml -i inventory -K
